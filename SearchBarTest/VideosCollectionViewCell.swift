61210626d65e5b2bccdc99cb3aba021d072b6a59
Eryus Developer <eryusdev@eryushions-mac-mini.local>
2017-08-02 13:36:34 +0530

    First Commit

---
+++ SearchBarTest/VideosCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  VideosCollectionViewCell.swift
+//  SearchBarTest
+//
+//  Created by Eryus Developer on 02/08/17.
+//  Copyright © 2017 Eryushion Techsol. All rights reserved.
+//
+
+import UIKit
+
+class VideosCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var videosImageView: UIImageView!
+    @IBOutlet weak var videosTitle: UILabel!
+}
